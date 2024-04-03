30e1e2755219ce037003360fb5ed0311d237bfd6
VanAdcox <vanadcox@gmail.com>
2024-04-03 17:52:53 -0500

    make color class more pythonic & implement closest color fully

--- Colors.py
+++ Colors.py
@@ -1,46 +1,57 @@
 import math,sys
 class Colors:
     COLORS = {
-    'BLACK' : ['\033[30m',(0,0,0)]
-    'RED' : ['\033[31m', (197,15,31)]
-    'GREEN' : ['\033[32m', (19,161,14)]
-    'YELLOW' : ['\033[33m', (193,156,0)]
-    'BLUE' : ['\033[34m', (0,55,218)]
-    'MAGENTA' : ['\033[35m', (178,0,178)]
-    'CYAN' : ['\033[36m', (58,150,221)]
-    'LIGHT_GRAY' : ['\033[37m', (118,118,118)]
-    'DARK_GRAY' : ['\033[90m', (204,204,204)]
-    'BRIGHT_RED' : ['\033[91m', (231,72,86)]
-    'BRIGHT_GREEN' : ['\033[92m', (22,198,12)]
-    'BRIGHT_YELLOW' : ['\033[93m', (249,241,165)]
-    'BRIGHT_BLUE' : ['\033[94m', (59,120,255)]
-    'BRIGHT_MAGENTA' : ['\033[95m', (180,0,158)]
-    'BRIGHT_CYAN' : ['\033[96m', (41,184,219)]
-    'WHITE' : ['\033[97m',(255,255,255)]
+        'BLACK' : '\033[30m',
+        'RED' : '\033[31m',
+        'GREEN' : '\033[32m',
+        'YELLOW' : '\033[33m',
+        'BLUE' : '\033[34m',
+        'MAGENTA' : '\033[35m',
+        'CYAN' : '\033[36m',
+        'LIGHT_GRAY' : '\033[37m',
+        'DARK_GRAY' : '\033[90m',
+        'BRIGHT_RED' : '\033[91m',
+        'BRIGHT_GREEN' : '\033[92m',
+        'BRIGHT_YELLOW' : '\033[93m',
+        'BRIGHT_BLUE' : '\033[94m',
+        'BRIGHT_MAGENTA' : '\033[95m',
+        'BRIGHT_CYAN' : '\033[96m',
+        'WHITE' : '\033[97m',
     }
-    
-    RESET = '\033[0m' 
-    RGB_PALETTE = []
-    
-    def __init__(self):
-        rgb_pallette = []
-        for atr in Colors.__dict__.keys():
-            if atr.__contains__('RGB'):
-                rgb_pallette.append(Colors.__dict__.get(atr))
-        self.RGB_PALETTE = rgb_pallette
-        
- 
-    def clrPrint(self, color, text):
-        print(f"{color}{text}{self.RESET}")
-    
-    def get_closest_color(self, input_rgb):
-        closest_color = self.BLACK_RGB
-        closest_dist = sys.maxsize 
-        for rgb_color in self.RGB_PALETTE:
-            dist = math.sqrt((rgb_color[0]-input_rgb[0]) + (rgb_color[1]-input_rgb[1]) + (rgb_color[2]-input_rgb[2]))
-            
-            if dist < closest_dist:
-                closest_dist = dist
-                closest_color = rgb_color
-        return closest_color
 
+    COLORS_RGB = {
+        'BLACK' : (0, 0, 0), 
+        'RED' : (197, 15, 31), 
+        'GREEN' : (19, 161, 14), 
+        'YELLOW' : (193, 156, 0), 
+        'BLUE' : (0, 55, 218), 
+        'MAGENTA' : (178, 0, 178), 
+        'CYAN' : (58, 150, 221), 
+        'LIGHT_GRAY' : (118, 118, 118), 
+        'DARK_GRAY' : (204, 204, 204), 
+        'BRIGHT_RED' : (231, 72, 86), 
+        'BRIGHT_GREEN' : (22, 198, 12), 
+        'BRIGHT_YELLOW' : (249, 241, 165), 
+        'BRIGHT_BLUE' : (59, 120, 255), 
+        'BRIGHT_MAGENTA' : (180, 0, 158), 
+        'BRIGHT_CYAN' : (41, 184, 219), 
+        'WHITE' : (255, 255, 255), 
+    }
+
+    RESET = '\033[0m' 
+ 
+def clrPrint(color, text):
+    print(f"{color}{text}{Colors.RESET}")
+
+def get_closest_color(input_rgb):
+    closest_color = Colors.COLORS_RGB.get('BLACK')
+    closest_dist = sys.maxsize 
+
+    for rgb_color in Colors.COLORS_RGB.values():
+        dist = math.sqrt(abs((rgb_color[0]-input_rgb[0]) + (rgb_color[1]-input_rgb[1]) + (rgb_color[2]-input_rgb[2])))
+        
+        if dist < closest_dist:
+            closest_dist = dist
+            closest_color = rgb_color
+    # TW: bad code  | Creates a list of keys and goes to the index of closest_color in a list of values
+    return (list(Colors.COLORS_RGB.keys())[list(Colors.COLORS_RGB.values()).index(closest_color)])
