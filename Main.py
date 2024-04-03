b42e6ee5f63ab89abb4a22cdf010d31b3626e81c
VanAdcox <vanadcox@gmail.com>
2024-04-02 20:15:15 -0500

    pull into image class & implement color class

--- Main.py
+++ Main.py
@@ -4,6 +4,98 @@ from glob import glob
 import os
 
 
+class Colors:
+    BLACK, BLACK_RGB = ('\033[30m',(0,0,0))
+    RED, RED_RGB = ('\033[31m', (197,15,31))
+    GREEN, GREEN_RGB = ('\033[32m', (19,161,14))
+    YELLOW, YELLOW_RGB = ('\033[33m', (193,156,0))
+    BLUE, BLUE_RGB = ('\033[34m', (0,55,218))
+    MAGENTA, MAGENTA_RGB = ('\033[35m', (178,0,178))
+    CYAN, CYAN_RGB = ('\033[36m', (58,150,221))
+    LIGHT_GRAY, LIGHT_GRAY_RGB = ('\033[37m', (118,118,118))
+    DARK_GRAY, DARK_GRAY_RGB = ('\033[90m', (204,204,204))
+    BRIGHT_RED, BRIGHT_RED_RGB = ('\033[91m', (231,72,86))
+    BRIGHT_GREEN, BRIGHT_GREEN_RGB = ('\033[92m', (22,198,12))
+    BRIGHT_YELLOW, BRIGHT_YELLOW_RGB = ('\033[93m', (249,241,165))
+    BRIGHT_BLUE, BRIGHT_BLUE_RGB = ('\033[94m', (59,120,255))
+    BRIGHT_MAGENTA, BRIGHT_MAGENTA = ('\033[95m', (180,0,158))
+    BRIGHT_CYAN,BRIGHT_CYAN = ('\033[96m', (41,184,219))
+    WHITE, WHITE_RGB = ('\033[97m',(255,255,255))
+
+    RESET = '\033[0m' 
+
+    def clrPrint(self, color, text):
+        print(f"{color}{text}{self.RESET}")
+
+
+class Proccessed_Image:
+    def __init__(self, image_file):
+        self.image = Image.open(image_file).resize((128,128))
+        self.grayscale = ImageOps.grayscale(self.image)
+        self.rgb_array = np.array(self.image)
+        self.gray_array = np.array(self.grayscale)
+    def image_to_ascii_array(self):
+        image_array = []
+        for row in self.gray_array:
+            new_row = []
+            for pixel_value in row:
+                if pixel_value <= 15:
+                    new_row.append('█')
+                    continue
+                if pixel_value <= 30:
+                    new_row.append('▓')
+                    continue
+                if pixel_value <= 45:
+                    new_row.append('▐')
+                    continue
+                if pixel_value <= 60:
+                    new_row.append('#')
+                    continue
+                if pixel_value <= 75:
+                    new_row.append('@')
+                    continue
+                if pixel_value <= 90:
+                    new_row.append('▒')
+                    continue
+                if pixel_value <= 105:
+                    new_row.append('0')
+                    continue
+                if pixel_value <= 120:
+                    new_row.append('%')
+                    continue
+                if pixel_value <= 135:
+                    new_row.append('=')
+                    continue
+                if pixel_value <= 150:
+                    new_row.append('+')
+                    continue
+                if pixel_value <= 165:
+                    new_row.append('-')
+                    continue
+                if pixel_value <= 180:
+                    new_row.append('_')
+                    continue
+                if pixel_value <= 195:
+                    new_row.append('|')
+                    continue
+                if pixel_value <= 210:
+                    new_row.append('!')
+                    continue
+                if pixel_value <= 225:
+                    new_row.append('\'')
+                    continue
+                if pixel_value <= 240:
+                    new_row.append('.')
+                    continue
+                if pixel_value <= 255:
+                    new_row.append(' ')
+                    continue
+                
+            image_array.append(new_row)
+        return image_array
+
+
+
 def process_images():
     raw_images = glob(os.path.join(f"{os.getcwd()}\\images\\", "*.png"))
     processed_images = []
@@ -13,66 +105,6 @@ def process_images():
 
         processed_images.append(np.array(image_grayscale))
     return processed_images
-
-def image_to_ascii(input_array):
-    image_array = []
-    for row in input_array:
-        new_row = []
-        for pixel_value in row:
-            if pixel_value <= 15:
-                new_row.append('█')
-                continue
-            if pixel_value <= 30:
-                new_row.append('▓')
-                continue
-            if pixel_value <= 45:
-                new_row.append('▐')
-                continue
-            if pixel_value <= 60:
-                new_row.append('#')
-                continue
-            if pixel_value <= 75:
-                new_row.append('@')
-                continue
-            if pixel_value <= 90:
-                new_row.append('▒')
-                continue
-            if pixel_value <= 105:
-                new_row.append('0')
-                continue
-            if pixel_value <= 120:
-                new_row.append('%')
-                continue
-            if pixel_value <= 135:
-                new_row.append('=')
-                continue
-            if pixel_value <= 150:
-                new_row.append('+')
-                continue
-            if pixel_value <= 165:
-                new_row.append('-')
-                continue
-            if pixel_value <= 180:
-                new_row.append('_')
-                continue
-            if pixel_value <= 195:
-                new_row.append('|')
-                continue
-            if pixel_value <= 210:
-                new_row.append('!')
-                continue
-            if pixel_value <= 225:
-                new_row.append('\'')
-                continue
-            if pixel_value <= 240:
-                new_row.append('.')
-                continue
-            if pixel_value <= 255:
-                new_row.append(' ')
-                continue
-            
-        image_array.append(new_row)
-    return image_array
 
 def print_out_img(image_array):
     for row in image_array:
@@ -87,10 +119,4 @@ def output_to_txt(image_array, name):
             f.write(col)
         f.write("\n")
 
-if __name__ == "__main__":
-    images = process_images()
-    i = 0
-    for image in images:
-        i += 1
-        output_to_txt(image_to_ascii(image), f'{i}_output')
-    print("\nSuccessfully Outputed txt files\n")
+
